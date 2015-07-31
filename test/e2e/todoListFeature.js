e7d65051631af3e077efa4cad75a76cc5af96587
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-31 17:58:36 +0100

    Passes feature test for displaying task name once added

--- test/e2e/todoListFeature.js
+++ test/e2e/todoListFeature.js
@@ -1,7 +1,25 @@
 describe('Todo list', function() {
+
+  var addTaskBox = element(by.model('listCtrl.newTask'))
+  var addButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080');
+  })
+
   it('has a title', function() {
     browser.get('http://localhost:8080');
 
     expect(browser.getTitle()).toEqual('To do list');
   });
+
+  it('displays tasks', function() {
+
+    var tasks = element.all(by.repeater('task in listCtrl.taskList'))
+
+    addTaskBox.sendKeys('Find marbles');
+    addButton.click();
+    expect(tasks.get(0).getText()).toEqual('Find marbles');
+  });
+
 });

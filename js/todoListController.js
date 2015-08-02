fb53f1b8de368f6fb76fce1eaaffaa754dfd0290
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-08-02 20:05:39 +0100

    Tidy up

--- js/todoListController.js
+++ js/todoListController.js
@@ -4,15 +4,9 @@ todoList.controller('TodoListController', [function() {
   self.taskList = [];
 
   self.addTask = function() {
-    // var task = {title: self.newTask}
     (self.taskList).push({title: self.newTask, done: false});
     console.log(self.taskList);
-  }
-  //
-  // self.isChecked = function(index) {
-  //   self.taskList[index].done = true;
-  //   console.log(self.taskList[index]);
-  // }
+  };
 
   self.deleteDone = function() {
     var doneIndeces = [];
@@ -22,21 +16,9 @@ todoList.controller('TodoListController', [function() {
         doneIndeces.push(i);
       };
     };
-    
+
     doneIndeces.forEach(function(index) {
       self.taskList.splice(index, 1);
-    })
+    });
   };
-
-
-    // self.taskList.forEach
-    // $self.taskList.splice(self.taskList.indexOf(person), 1 );
-    // self.taskList = self.taskList.filter(function() {
-    //   return !task.done;
-    // })
-    //
-    // $filter('filter')(array, expression, comparator)
-
-
-
 }]);

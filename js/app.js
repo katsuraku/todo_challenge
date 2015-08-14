e7a78ff358672f574b16a80125d4731a78edc09f
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-08-14 13:50:42 +0100

    Made todo items editable on page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-var todoList  = angular.module('TodoList', ['ngResource']);
+var todoList  = angular.module('TodoList', ['ngResource', 'xeditable']);
+
+// .run(function(editableOptions) {
+//   editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+// });
